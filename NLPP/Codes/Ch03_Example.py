259c3707864c109943edab6c7e64def87313e132
sunyardCUI <1924375084@qq.com>
2023-04-27 17:41:09 +0800

    ch03 example finished

--- NLPP/Codes/Ch03_Example.py
+++ NLPP/Codes/Ch03_Example.py
@@ -11,6 +11,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch import Tensor
 import torch
+import os
+import re
 import pandas as pd
 import numpy as np
 import string
@@ -122,10 +124,10 @@ class ReviewDataSet(Dataset):
 
 class Vocabulary(object):
     def __init__(self,
-                 token_to_index: dict = None,
+                 token_to_idx: dict = None,
                  add_unk: bool = True,
                  unk_token: str = "<UNK>"):
-        if token_to_index is None:
+        if token_to_idx is None:
             token_to_idx = {}
         self._token_to_idx = token_to_idx
 
@@ -154,7 +156,7 @@ class Vocabulary(object):
             token (str): the item to add into the Vocabulary
 
         Returns:
-            int: the integer corresponding to the token 
+            int: the integer corresponding to the token
         """
         if token in self._token_to_idx:
             index = self._token_to_idx[token]
@@ -314,31 +316,63 @@ class ReviewClassifier(nn.Module):
         return y_out
 
 
+def set_seed_everywhere(seed, cuda):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed(seed)
+
+
 args = Namespace(
     # Data and path information
     frequency_cutoff=25,
     model_state_file='model.pth',
     review_csv='data/yelp/reviews_with_split_lite.csv',
     save_dir='model_storage/ch3/yelp/',
+    vectorizer_file='vectorizer.json',
     # No model hyperparameters
     # Training parameters
     batch_size=128,
     early_stopping_criteria=5,
     learning_rate=0.001,
-    num_epochs=1,
+    num_epochs=10,
     seed=1337,
-    # Runtime options omitted for space
+    # Runtime options
+    catch_keyboard_interrupt=True,
+    cuda=False,
+    expand_filepaths_to_save_dir=True,
+    reload_from_files=False
 )
+
+if args.expand_filepaths_to_save_dir:
+    args.vectorizer_file = os.path.join(args.save_dir,
+                                        args.vectorizer_file)
+    args.model_state_file = os.path.join(args.save_dir,
+                                         args.model_state_file)
+    print('Expanded filepaths')
+    print('\t{}'.format(args.vectorizer_file))
+    print('\t{}'.format(args.model_state_file))
+
+if not torch.cuda.is_available():
+    args.cuda = False
+print('Using CUDA: {}'.format(args.cuda))
+args.device = torch.device("cuda" if args.cuda else 'cpu')
+
+set_seed_everywhere(args.seed, args.cuda)
 
 
 def make_train_state(args) -> dict:
-    return {'epoch_index': 0,
+    return {'stop_early': False,
+            'early_stopping_step': 0,
+            'early_stopping_best_val': 1e8,
+            'epoch_index': 0,
             'train_loss': [],
             'train_acc': [],
             'val_loss': [],
             'val_acc': [],
             'test_loss': -1,
-            'test_acc': -1}
+            'test_acc': -1,
+            'model_filename': args.model_state_file}
 
 
 def compute_accuracy(y_pred: Tensor, y_target: Tensor) -> float:
@@ -349,15 +383,35 @@ def compute_accuracy(y_pred: Tensor, y_target: Tensor) -> float:
     return n_correct / len(y_pred_indices) * 100
 
 
+def update_train_state(args, model, train_state):
+    if train_state['epoch_index'] == 0:
+        torch.save(model.state_dict(), train_state['model_filename'])
+        train_state['stop_early'] = False
+    elif train_state['epoch_index'] >= 1:
+        loss_tml, loss_t = train_state['val_loss'][-2:]
+        if loss_t >= train_state['early_stopping_best_val']:
+            train_state['early_stopping_step'] += 1
+        else:
+            if loss_t < train_state['early_stopping_best_val']:
+                torch.save(model.state_dict(), train_state['model_filename'])
+            train_state['early_stopping_step'] = 0
+        train_state['stop_early'] = train_state['early_stopping_step'] >= args.early_stopping_criteria
+
+    return train_state
+
+
 train_state = make_train_state(args)
-if not torch.cuda.is_available():
-    args.cuda = False
-args.device = torch.device("cuda" if args.cuda else 'cpu')
 
-# 数据集和分词器
-dataset = ReviewDataSet.load_dataset_and_make_vectorizer(args.review_csv)
+# 初始化
+if args.reload_from_files:
+    print('Loading dataset and vectorizer')
+    dataset = ReviewDataSet.load_dataset_and_load_vectorizer(args.review_csv,
+                                                             args.vectorizer_file)
+else:
+    print("Loading dataset and creating vectorizer")
+    dataset = ReviewDataSet.load_dataset_and_make_vectorizer(args.review_csv)
+    dataset.save_vectorizer(args.vectorizer_file)
 vectorizer = dataset.get_vectorizer()
-
 # 模型
 classifier = ReviewClassifier(num_features=len(vectorizer.review_vocab))
 classifier = classifier.to(args.device)
@@ -366,8 +420,11 @@ classifier = classifier.to(args.device)
 loss_func = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(classifier.parameters(), lr=args.learning_rate)
 
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,
+                                                 mode='min', factor=.5,
+                                                 patience=1)
 epoch_bar = tqdm(desc='training routine',
-                 total=args.num_epochs,
+                 total=2 * args.num_epochs,
                  position=0)
 
 dataset.set_split('train')
@@ -375,6 +432,12 @@ train_bar = tqdm(desc='split=train',
                  total=dataset.get_num_batches(args.batch_size),
                  position=1,
                  leave=True)
+
+dataset.set_split('val')
+val_bar = tqdm(desc='split=val',
+               total=dataset.get_num_batches(args.batch_size),
+               position=1,
+               leave=True)
 
 for epoch_idx in range(args.num_epochs):
     train_state['epoch_index'] = epoch_idx
@@ -420,24 +483,121 @@ for epoch_idx in range(args.num_epochs):
     batch_generator = generate_batches(dataset,
                                        batch_size=args.batch_size,
                                        device=args.device)
-# running_loss = 0.
-# running_acc = 0.
-# classifier.eval()
+    running_loss = 0.
+    running_acc = 0.
+    classifier.eval()
 
-# for batch_idx, batch_dict in enumerate(batch_generator):
-#     # step 1: compute the output
-#     y_pred = classifier(x_in=batch_dict['x_data'].float())
+    for batch_idx, batch_dict in enumerate(batch_generator):
+        # step 1: compute the output
+        y_pred = classifier(x_in=batch_dict['x_data'].float())
 
-#     # step 2: compute the loss
-#     loss = loss_func(y_pred, batch_dict['y_target'].float())
-#     loss_batch = loss.item()
-#     running_loss += (loss_batch - running_loss) / (batch_idx + 1)
+        # step 2: compute the loss
+        loss = loss_func(y_pred, batch_dict['y_target'].float())
+        loss_batch = loss.item()
+        running_loss += (loss_batch - running_loss) / (batch_idx + 1)
 
-#     # step 3 compute the accuracy
-#     acc_batch = compute_accuracy(y_pred, batch_dict['y_target'].float())
-#     running_acc += (acc_batch - running_acc) / (batch_idx + 1)
+        # step 3 compute the accuracy
+        acc_batch = compute_accuracy(y_pred, batch_dict['y_target'].float())
+        running_acc += (acc_batch - running_acc) / (batch_idx + 1)
 
-# train_state['val_loss'].append(running_loss)
-# train_state['val_acc'].append(running_acc)
+        val_bar.set_postfix(loss=running_loss,
+                            acc=running_acc,
+                            epoch=epoch_idx)
+        val_bar.update()
+
+    train_state['val_loss'].append(running_loss)
+    train_state['val_acc'].append(running_acc)
+
+    train_state = update_train_state(args=args, model=classifier,
+                                     train_state=train_state)
+    scheduler.step(train_state['val_loss'][-1])
     train_bar.n = 0
+    val_bar.n = 0
     epoch_bar.update()
+    if train_state['stop_early']:
+        break
+
+    train_bar.n = 0
+    val_bar.n = 0
+    epoch_bar.update()
+
+
+# compute the loss and accuracy on the test set using the best available model
+classifier.load_state_dict(torch.load(train_state['model_filename']))
+classifier = classifier.to(args.device)
+
+dataset.set_split('test')
+batch_generator = generate_batches(dataset,
+                                   batch_size=args.batch_size,
+                                   device=args.device)
+running_loss = 0.
+running_acc = 0.
+classifier.eval()
+
+for batch_idx, batch_dict in enumerate(batch_generator):
+    y_pred = classifier(x_in=batch_dict['x_data'].float())
+
+    loss = loss_func(y_pred, batch_dict['y_target'].float())
+    loss_t = loss.item()
+    running_loss += (loss_t - running_loss) / (batch_idx + 1)
+    acc_t = compute_accuracy(y_pred, batch_dict['y_target'])
+    running_acc += (acc_t - running_acc) / (batch_idx + 1)
+
+train_state['test_acc'] = running_loss
+train_state['test_acc'] = running_acc
+
+print("Test loss: {:.3f}".format(train_state['test_loss']))
+print('Test Accuracy: {:.2f}'.format(train_state['test_acc']))
+
+# inference
+
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r"([.,!?])", r" \1 ", text)
+    text = re.sub(r"[^a-zA-Z.,!?]+", r" ", text)
+    return text
+
+
+def predict_rating(review: str, classifier: ReviewClassifier,
+                   vectorizer: ReviewVectorizer, decision_threshold: float = .5):
+    review = preprocess_text(review)
+    vectorized_review = torch.tensor(vectorizer.vectorize(review))
+    result = classifier(vectorized_review.view(1, -1))
+    probability_value = F.sigmoid(result).item()
+    index = 1
+    if probability_value < decision_threshold:
+        index = 0
+
+    return vectorizer.rating_vocab.lookup_index(index)
+
+
+test_review = "this is a pretty awesome book"
+
+classifier = classifier.cpu()
+prediction = predict_rating(test_review,
+                            classifier=classifier,
+                            vectorizer=vectorizer,
+                            decision_threshold=.5)
+print('{} -> {}'.format(test_review, prediction))
+
+# interpret ability
+print(classifier.fc1.weight.shape)
+
+fc1_weights = classifier.fc1.weight.detach()[0]
+_, indices = torch.sort(fc1_weights, dim=0, descending=True)
+indices = indices.numpy().tolist()
+
+# Top 20 positive words
+print('Influential words in Positive Reviews')
+print('-------------------------------------')
+for i in range(20):
+    print(vectorizer.review_vocab.lookup_index(indices[i]))
+print('\n')
+# Top 20 negative words
+print('Influential words in Negative Reviews')
+print('-------------------------------------')
+indices.reverse()
+for i in range(20):
+    print(vectorizer.review_vocab.lookup_index(indices[i]))
+print('\n')
